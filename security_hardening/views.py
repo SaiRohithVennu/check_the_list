35e9288e3df717716fa9280791ecc691b243f70c
nguonsg <nguonsg@mail.uc.edu>
2025-04-03 00:28:04 -0400

    update on view

--- security_hardening/views.py
+++ security_hardening/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import authenticate, login, logout, login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from rest_framework import viewsets
 from .models import SecurityCheck
